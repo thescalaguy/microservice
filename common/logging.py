a2db7e1c4a985666c0c9db84ec17cda61ec55432
Fasih Khatib <hellofasih.confound928@passinbox.com>
2025-02-16 08:34:40 +0530

    Change logging format

--- common/logging.py
+++ common/logging.py
@@ -1,21 +1,15 @@
-import os
 import logging
-
-from pythonjsonlogger.json import JsonFormatter
+import os
 from opentelemetry._logs import set_logger_provider
-from opentelemetry.sdk._logs.export import ConsoleLogExporter
 from opentelemetry.exporter.otlp.proto.grpc._log_exporter import OTLPLogExporter
 from opentelemetry.sdk._logs import LoggerProvider, LoggingHandler
-from opentelemetry.sdk._logs.export import BatchLogRecordProcessor, SimpleLogRecordProcessor
+from opentelemetry.sdk._logs.export import SimpleLogRecordProcessor
 from opentelemetry.sdk.resources import Resource
-from pythonjsonlogger.json import JsonFormatter
-from flask import Flask
-
 
 
 def init_logger():
     endpoint = "localhost:4317"
-    
+
     logger_provider = LoggerProvider(
         resource=Resource.create(
             {
@@ -30,11 +24,18 @@ def init_logger():
     otlp_exporter = OTLPLogExporter(endpoint=endpoint, insecure=True)
     # logger_provider.add_log_record_processor(BatchLogRecordProcessor(otlp_exporter))
     logger_provider.add_log_record_processor(SimpleLogRecordProcessor(otlp_exporter))
-    
+
     # console_exporter = ConsoleLogExporter()
     # logger_provider.add_log_record_processor(BatchLogRecordProcessor(console_exporter))
 
-    formatter = JsonFormatter()
+    # formatter = JsonFormatter()
+
+    formatter = logging.Formatter(
+        "{asctime} - {levelname} - {message}",
+        style="{",
+        datefmt="%Y-%m-%d %H:%M",
+    )
+
     handler = LoggingHandler(level=logging.INFO, logger_provider=logger_provider)
     handler.setFormatter(formatter)
 
